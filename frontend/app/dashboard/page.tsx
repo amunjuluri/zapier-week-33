49e3e3699cf7a0677c7e3348f14b07b6a3483a85
anand munjuluri <munjulurianand@gmail.com>
2024-10-26 07:32:52 +0530

    Update page.tsx

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -84,14 +84,14 @@ function ZapTable({ zaps }: {zaps: Zap[]}) {
         <div className="flex">
                 <div className="flex-1">Name</div>
                 <div className="flex-1">ID</div>
-                <div className="flex-1">Created at</div>
+         
                 <div className="flex-1">Webhook URL</div>
-                <div className="flex-1">Go</div>
+                
         </div>
         {zaps.map(z => <div className="flex border-b border-t py-4">
             <div className="flex-1 flex"><img src={z.trigger.type.image} className="w-[30px] h-[30px]" /> {z.actions.map(x => <img src={x.type.image} className="w-[30px] h-[30px]" />)}</div>
             <div className="flex-1">{z.id}</div>
-            <div className="flex-1">Nov 13, 2023</div>
+            
             <div className="flex-1">{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</div>
             <div className="flex-1"><LinkButton onClick={() => {
                     router.push("/zap/" + z.id)
